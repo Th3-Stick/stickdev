5bccbca8c4e95368c59b4996303846f41f8a4314
Th3-Stick <delaigue.adrien@gmail.com>
2023-06-15 17:18:51 +0200

    JS pour toutes les pages OK / A optimiser

--- js/nav.js
+++ js/nav.js
@@ -59,3 +59,76 @@ const portfolio = {
 };
 
 portfolio.init();
+
+const projets = {
+    init: function(){
+        projets.pages = document.querySelectorAll('.page')
+        projets.cible = document.querySelector('.projets');
+        projets.onglets = document.querySelector('#link_projets');
+        projets.onglets.addEventListener('click', projets.handleclick);
+    },
+    handleclick: function(event) {
+        event.preventDefault();
+        projets.pages.forEach(function(page) {
+            page.classList.remove('show');
+        });
+        projets.cible.classList.add('show');
+    }
+};
+
+projets.init();
+
+const about = {
+    init: function(){
+        about.pages = document.querySelectorAll('.page')
+        about.cible = document.querySelector('.about');
+        about.onglets = document.querySelector('#link_about');
+        about.onglets.addEventListener('click', about.handleclick);
+    },
+    handleclick: function(event) {
+        event.preventDefault();
+        about.pages.forEach(function(page) {
+            page.classList.remove('show');
+        });
+        about.cible.classList.add('show');
+    }
+};
+
+about.init();
+
+
+const blog = {
+    init: function(){
+        blog.pages = document.querySelectorAll('.page')
+        blog.cible = document.querySelector('.blog');
+        blog.onglets = document.querySelector('#link_blog');
+        blog.onglets.addEventListener('click', blog.handleclick);
+    },
+    handleclick: function(event) {
+        event.preventDefault();
+        blog.pages.forEach(function(page) {
+            page.classList.remove('show');
+        });
+        blog.cible.classList.add('show');
+    }
+};
+
+blog.init();
+
+const contact = {
+    init: function(){
+        contact.pages = document.querySelectorAll('.page')
+        contact.cible = document.querySelector('.contact');
+        contact.onglets = document.querySelector('#link_contact');
+        contact.onglets.addEventListener('click', contact.handleclick);
+    },
+    handleclick: function(event) {
+        event.preventDefault();
+        contact.pages.forEach(function(page) {
+            page.classList.remove('show');
+        });
+        contact.cible.classList.add('show');
+    }
+};
+
+contact.init();
