48706151f19213e94f303cb993876f600f2dc533
Th3-Stick <delaigue.adrien@gmail.com>
2023-06-17 00:30:27 +0200

    Optimisation JS dans une seule fonction avec foreach + event target ID

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,14 @@
 const nav = {
-    links: null,
-
     init: function() {
+        nav.pages = document.querySelectorAll('.page')
+
+        nav.homePage = document.querySelector('.home');
+        nav.portfolio = document.querySelector('.portfolio');
+        nav.projets = document.querySelector('.projets');
+        nav.about = document.querySelector('.about');
+        nav.blog = document.querySelector('.blog');
+        nav.contact = document.querySelector('.contact');
+
         nav.links = document.querySelectorAll('.navbar-nav a');
         nav.links.forEach(function(link) {
             link.addEventListener("click", nav.handleClickNav);
@@ -14,121 +21,27 @@ const nav = {
             link.classList.remove('active');
         });
         this.classList.add('active');
-    },
+
+        nav.pages.forEach(function(page) {
+            page.classList.remove('show');
+        });
+        if (event.target.id === 'link_home') {
+            nav.homePage.classList.add('show');
+        } else if (event.target.id === 'link_portfolio')  {
+            nav.portfolio.classList.add('show');
+        } else if (event.target.id === 'link_projets') {
+            nav.projets.classList.add('show');
+        } else if (event.target.id === 'link_about') {
+            nav.about.classList.add('show');
+        } else if (event.target.id === 'link_blog') {
+            nav.blog.classList.add('show');
+        } else if (event.target.id === 'link_contact') {
+            nav.contact.classList.add('show');
+        } 
+    }
 };
 
 
 document.addEventListener('DOMContentLoaded', function() {
     nav.init();
 });
-
-
-const home = {
-    init: function(){
-        home.pages = document.querySelectorAll('.page')
-        home.homePage = document.querySelector('.home');
-        home.onglets = document.querySelector('#link_home');
-        home.onglets.addEventListener('click', home.handleclick);
-    },
-    handleclick: function(event) {
-        event.preventDefault();
-        home.pages.forEach(function(page) {
-            page.classList.remove('show');
-        });
-        home.homePage.classList.add('show');
-    }
-};
-
-home.init();
-
-
-const portfolio = {
-    init: function(){
-        portfolio.pages = document.querySelectorAll('.page')
-        portfolio.homePage = document.querySelector('.portfolio');
-        portfolio.onglets = document.querySelector('#link_portfolio');
-        portfolio.onglets.addEventListener('click', portfolio.handleclick);
-    },
-    handleclick: function(event) {
-        event.preventDefault();
-        portfolio.pages.forEach(function(page) {
-            page.classList.remove('show');
-        });
-        portfolio.homePage.classList.add('show');
-    }
-};
-
-portfolio.init();
-
-const projets = {
-    init: function(){
-        projets.pages = document.querySelectorAll('.page')
-        projets.cible = document.querySelector('.projets');
-        projets.onglets = document.querySelector('#link_projets');
-        projets.onglets.addEventListener('click', projets.handleclick);
-    },
-    handleclick: function(event) {
-        event.preventDefault();
-        projets.pages.forEach(function(page) {
-            page.classList.remove('show');
-        });
-        projets.cible.classList.add('show');
-    }
-};
-
-projets.init();
-
-const about = {
-    init: function(){
-        about.pages = document.querySelectorAll('.page')
-        about.cible = document.querySelector('.about');
-        about.onglets = document.querySelector('#link_about');
-        about.onglets.addEventListener('click', about.handleclick);
-    },
-    handleclick: function(event) {
-        event.preventDefault();
-        about.pages.forEach(function(page) {
-            page.classList.remove('show');
-        });
-        about.cible.classList.add('show');
-    }
-};
-
-about.init();
-
-
-const blog = {
-    init: function(){
-        blog.pages = document.querySelectorAll('.page')
-        blog.cible = document.querySelector('.blog');
-        blog.onglets = document.querySelector('#link_blog');
-        blog.onglets.addEventListener('click', blog.handleclick);
-    },
-    handleclick: function(event) {
-        event.preventDefault();
-        blog.pages.forEach(function(page) {
-            page.classList.remove('show');
-        });
-        blog.cible.classList.add('show');
-    }
-};
-
-blog.init();
-
-const contact = {
-    init: function(){
-        contact.pages = document.querySelectorAll('.page')
-        contact.cible = document.querySelector('.contact');
-        contact.onglets = document.querySelector('#link_contact');
-        contact.onglets.addEventListener('click', contact.handleclick);
-    },
-    handleclick: function(event) {
-        event.preventDefault();
-        contact.pages.forEach(function(page) {
-            page.classList.remove('show');
-        });
-        contact.cible.classList.add('show');
-    }
-};
-
-contact.init();
